a3784629577e084da37d5d60212fb6d0761dd3d9
Igor Tkachenko <vash.igor@gmail.com>
2017-03-23 17:49:00 +0300

    Update clickCounter.ts

--- clickCounter.ts
+++ clickCounter.ts
@@ -1,5 +1,6 @@
 export class ClickCounter {
     clickCounterFunc (someArg, callSomeFunc, value) {
+        // Use a function from the object here.
         return [someArg + 1, callSomeFunc(value)];
     }
 }
